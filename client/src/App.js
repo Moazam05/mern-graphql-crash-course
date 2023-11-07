3249146e7d50cfba6fa15c0c34759b68e3f257b9
Moazam05 <salmanmoazam08@gmail.com>
2023-11-07 22:08:58 +0500

    apollo client query and mutation

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,42 @@
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+// Custom Imports
 import Header from "./components/Header";
+import Clients from "./components/Clients";
+
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+      },
+    },
+  },
+});
+
+const client = new ApolloClient({
+  uri: `http://localhost:5000/graphql`,
+  // cache: new InMemoryCache(),
+  cache,
+});
 
 const App = () => {
   return (
     <>
-      <Header />
-      <div className="container">
-        <h1>Hello World</h1>
-      </div>
+      <ApolloProvider client={client}>
+        <Header />
+        <div className="container">
+          <Clients />
+        </div>
+      </ApolloProvider>
     </>
   );
 };
---
+++ client/src/components/Clients/index.jsx
@@ -0,0 +1,36 @@
+import { useQuery } from "@apollo/client";
+// Custom Imports
+import ClientRow from "../ClientRow";
+import { GET_CLIENTS } from "../../queries/clientQueries";
+import Spinner from "../Spinner";
+
+const Clients = () => {
+  const { loading, error, data } = useQuery(GET_CLIENTS);
+
+  if (loading) return <Spinner />;
+  if (error) return <p>Error :</p>;
+
+  return (
+    <>
+      {!loading && !error && (
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Name</th>
+              <th scope="col">Email</th>
+              <th scope="col">Phone</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.clients.map((client) => (
+              <ClientRow key={client.id} client={client} />
+            ))}
+          </tbody>
+        </table>
+      )}
+    </>
+  );
+};
+
+export default Clients;
