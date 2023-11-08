b6264c34ee8184417e29e1f2f27972c5903ef407
Moazam05 <salmanmoazam08@gmail.com>
2023-11-08 15:49:17 +0500

    delete project api bind

--- client/src/views/Projects/Components/DeleteProjectButton.jsx
+++ client/src/views/Projects/Components/DeleteProjectButton.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { FaTrash } from "react-icons/fa";
+import { DELETE_PROJECT } from "../../Projects/graphql/mutations/clientMutations";
+import { GET_PROJECTS } from "../../Projects/graphql/queries/projectQueries";
+import { useMutation } from "@apollo/client";
 
 const DeleteProjectButton = ({ projectId }) => {
-  return <div>DeleteProjectButton</div>;
+  const navigate = useNavigate();
+
+  const [deleteProject] = useMutation(DELETE_PROJECT, {
+    variables: { id: projectId },
+    onCompleted: () => navigate("/"),
+    refetchQueries: [{ query: GET_PROJECTS }],
+  });
+
+  return (
+    <div className="d-flex mt-5 ms-auto">
+      <button className="btn btn-danger m-2" onClick={deleteProject}>
+        <FaTrash className="icon" /> Delete Project
+      </button>
+    </div>
+  );
 };
 
 export default DeleteProjectButton;
